48a6cba90f544846d02b25f2142c7eb154d83401
Earth <pimanus1234@gmail.com>
2022-05-31 18:45:23 +0700

    Basic mui

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,27 +1,40 @@
-import { Button, Typography } from "@mui/material";
+import { Button,styled, Typography } from "@mui/material";
 import { Add, Settings } from "@mui/icons-material";
 
 function App() {
+
+  const BlueButton = styled(Button)(({ theme }) => ({
+    backgroundColor: "skyblue",
+    color: "#888",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue"
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white"
+    }
+  }));
+
   return (
     <div>
       <Button variant="text">Text</Button>
-      <Button startIcon = {<Settings/>} variant="contained">Contained</Button>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size = "small"
+      >Contained</Button>
       <Button startIcon = {<Add/>} color = "success" variant="contained">Add new post</Button>
       <Button variant="outlined">Outlined</Button>
 
       <Typography variant="h1" component="p">
         hh1 style but in p tag
       </Typography>;
-      <Button variant="contained" sx={{
-        backgroundColor: "skyblue",
-        color: "#888",
-        margin: 5,
-        "&:hover": {
-          backgroundColor:"lightblue"
-        }
-      }}>
-        My Unique Button
-      </Button>
+      <BlueButton>First Button</BlueButton>
+      <BlueButton>Another Button</BlueButton>
+
+
     </div>
   );
 }
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,12 +1,16 @@
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { theme } from "./theme";
 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+      <App />
+    
   </React.StrictMode>
 );
 
