bb928ce23e514c43a3c2566a4d5c26908055d1e1
Earth <pimanus1234@gmail.com>
2022-05-31 17:23:16 +0700

    init mui

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Typography } from "@mui/material";
+import { Add, Settings } from "@mui/icons-material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="text">Text</Button>
+      <Button startIcon = {<Settings/>} variant="contained">Contained</Button>
+      <Button startIcon = {<Add/>} color = "success" variant="contained">Add new post</Button>
+      <Button variant="outlined">Outlined</Button>
+
+      <Typography variant="h1" component="p">
+        hh1 style but in p tag
+      </Typography>;
+      <Button variant="contained" sx={{
+        backgroundColor: "skyblue",
+        color: "#888",
+        margin: 5,
+        "&:hover": {
+          backgroundColor:"lightblue"
+        }
+      }}>
+        My Unique Button
+      </Button>
     </div>
   );
 }
