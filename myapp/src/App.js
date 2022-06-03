b5e1da80b71294260b64f64c2c7fe716a6a534f2
Earth <pimanus1234@gmail.com>
2022-06-03 16:18:32 +0700

    Back to default App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,38 +1,9 @@
-import { Button,styled, Typography } from "@mui/material";
-import { Add, Settings } from "@mui/icons-material";
+
 
 function App() {
 
-  const BlueButton = styled(Button)(({ theme }) => ({
-    backgroundColor: "skyblue",
-    color: "#888",
-    margin: 5,
-    "&:hover": {
-      backgroundColor: "lightblue"
-    },
-    "&:disabled": {
-      backgroundColor: "gray",
-      color: "white"
-    }
-  }));
-
   return (
     <div>
-      <Button variant="text">Text</Button>
-      <Button
-        startIcon={<Settings />}
-        variant="contained"
-        color="secondary"
-        size = "small"
-      >Contained</Button>
-      <Button startIcon = {<Add/>} color = "success" variant="contained">Add new post</Button>
-      <Button variant="outlined">Outlined</Button>
-
-      <Typography variant="h1" component="p">
-        hh1 style but in p tag
-      </Typography>;
-      <BlueButton>First Button</BlueButton>
-      <BlueButton>Another Button</BlueButton>
 
 
     </div>
